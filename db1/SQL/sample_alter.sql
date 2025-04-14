8f0b5dcef11d79949a8202f1752925c6f0b6f08f
kktest25 <kolliparakarthik1991@gmail.com>
2025-04-14 18:16:23 +0530

    temp commit sql file

--- db1/SQL/sample_alter.sql
+++ db1/SQL/sample_alter.sql
@@ -3,3 +3,4 @@ second line in sql folder temp script
 third line in sql folder temp script
 fourth line in sql folder temp script
 fift line in sql folder temp script
+sixth line in sql folder temp script
