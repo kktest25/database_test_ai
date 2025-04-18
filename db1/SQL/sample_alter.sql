a31c5bb73320b1f4453e1f5f420aa25ed35902f7
kktest25 <kolliparakarthik1991@gmail.com>
2025-04-18 14:44:32 +0530

    temp script

--- db1/SQL/sample_alter.sql
+++ db1/SQL/sample_alter.sql
@@ -6,3 +6,4 @@ fift line in sql folder temp script
 sixth line in sql folder temp script
 seventh line in sql folder temp script
 8th line in sql folder temp script
+9th line in sql folder temp script
